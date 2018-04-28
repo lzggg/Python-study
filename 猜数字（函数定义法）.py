0652f270328ab5dc8ddad667bdf90d8654281a5d
lzggg <38792271+lzggg@users.noreply.github.com>
2018-04-28 03:12:35 -0500

    Add files via upload

---
+++ 猜数字（函数定义法）.py
@@ -0,0 +1,27 @@
+def bijiao(a,b):
+    if a < b:
+        print("%d is too big"%b)
+        return False
+    if a > b:
+        print("%d is too small"%b)
+        return False
+    if a == b:
+        print("bingo!You are right!Again?")
+        c = str(input())
+        if c in ("Yes","yes","Y","y"):
+            again()
+        else:
+            return True
+
+def again():
+    print("Give me a number")
+    a = randint(0,1000)
+    answer = False
+    while answer == False:
+        b = int(input())
+        answer = bijiao(a,b)
+    print("Byebye!")
+
+print("What am I?(1~1000)")
+from random import *
+again()
